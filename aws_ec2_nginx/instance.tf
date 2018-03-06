50946cd6e368c3f69e30a82479798b1b20307571
heowc <heowc1992@gmail.com>
2018-03-06 18:58:14 +0900

    Fix: [nginx, vpc advanced] - key 분리 - security group에서 인바운드 수정

--- aws_ec2_nginx/instance.tf
+++ aws_ec2_nginx/instance.tf
@@ -1,8 +1,3 @@
-resource "aws_key_pair" "mykey" {
-  key_name = "mykey"
-  public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
-}
-
 resource "aws_instance" "example" {
   ami = "${lookup(var.AWS_AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
